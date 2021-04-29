ce03047f845057986a2e47aa06f85b82145e9593
Hullicatantus <nsl@freemail.hu>
2021-04-29 11:54:36 +0200

    The most 'streamish' solution

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                Files.write(Path.of(output), () -> contacts.entrySet().stream()
+                        .<CharSequence>map(e -> e.getKey() + ": " + e.getValue())
+                        .iterator());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+}
